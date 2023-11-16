ba03071c66c5608a79fdcdec07fbe83b81f4204b
Thiago Martins <thiagolealmartins@gmail.com>
2023-11-16 00:34:21 -0300

    feat: adicionado banco H2 para testes de integração

--- CRUD/src/test/java/com/ThiagoLMartins/CRUD/repository/UsuarioRepositoryTest.java
+++ CRUD/src/test/java/com/ThiagoLMartins/CRUD/repository/UsuarioRepositoryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.ThiagoLMartins.CRUD.modal.Usuario;
@@ -21,6 +22,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @ExtendWith(value = SpringExtension.class)
+@ActiveProfiles("testes")
 public class UsuarioRepositoryTest {
 
 	@Autowired
